c61d326d9d8f4631c97fa5e0d9fdc72ce5b0b8be
Frederick Cai <frederick.cai@gmail.com>
2016-01-21 21:26:34 +1300

    Add lib compile config

--- gulpfile.js
+++ gulpfile.js
@@ -7,82 +7,57 @@ const tsc    = require('gulp-typescript')
 const mocha  = require('gulp-mocha')
 const args   = require('yargs').argv
 
-function onError(error) {
-  notify.onError({
-    title: error.name || 'Error',
-    message: error.message || error || '',
-  })
-  console.log(error.stack)
-  this.emit('end')
-}
-
-const paths = {
-  lib: {
-    all:  './src/**/*.{ts,js}',
-    dest: './lib'
-  },
-  test: {
-    all:  './test/**/*.{ts,js}',
-    dest: './dist/test'
-  },
-}
-
-const mainProject = tsc.createProject(
-  'src/tsconfig.json', {
-    noExternalResolve: true
-  })
-
-const testProject = tsc.createProject(
-  'test/tsconfig.json', {
-    noExternalResolve: true
-  })
-
-const mochaConfig = {
-  reporter: 'spec',
-  globals: {
-    should: require('should')
-  },
-  bail: !!args.bail,
-}
-
-// ----------------------------------------------------------------------------
-// Clean
-
 gulp.task('clean', () => {
   return gulp.src(['dist', 'lib', 'coverage'], { read: false })
     .pipe(rimraf())
 })
 
 // ----------------------------------------------------------------------------
-// Build Source
 
 gulp.task('build:lib', () => {
-  return mainProject.src()
-    .pipe(tsc(mainProject))
+  return gulp.src('src/**/*.ts')
+    .pipe(tsc({
+      target: 'es6',
+      module: 'commonjs',
+      removeComments: true,
+      preserveConstEnums: true,
+      sourceMap: false,
+      noExternalResolve: true,
+      out: 'index.js'
+    }))
     .on('error', onError)
     .pipe(gulp.dest('lib'))
 })
 
-gulp.task('build', ['clean', 'build:lib'])
-
-// ----------------------------------------------------------------------------
-// Build and Run Test
-
 gulp.task('build:test', () => {
-  return testProject.src()
-    .pipe(tsc(testProject))
+  return gulp.src('test/**/*.ts')
+    .pipe(tsc({
+      target: 'es6',
+      module: 'commonjs',
+      removeComments: true,
+      preserveConstEnums: true,
+      sourceMap: false,
+      noExternalResolve: true
+    }))
     .on('error', onError)
     .pipe(gulp.dest('dist/test'))
 })
 
+gulp.task('build', ['clean', 'build:lib'])
+
 gulp.task('test', (done) => {
-  return gulp.src('./dist/test/**/*.js', { read: false })
-    .pipe(mocha(mochaConfig))
+  return gulp.src('dist/test/**/*.js', { read: false })
+    .pipe(mocha({
+      reporter: 'spec',
+      globals: {
+        should: require('should')
+      },
+      bail: !!args.bail,
+    }))
     .on('error', onError)
 })
 
 // ----------------------------------------------------------------------------
-// Watch
 
 gulp.task('watch:build', ['build'], () => {
   gulp.watch(['src/**/*.{ts}'], ['build'])
@@ -93,3 +68,14 @@ gulp.task('watch:test', ['test'], () => {
 })
 
 gulp.task('watch', ['watch:build', 'watch:test'])
+
+// ----------------------------------------------------------------------------
+
+function onError(error) {
+  notify.onError({
+    title: error.name || 'Error',
+    message: error.message || error || '',
+  })
+  console.log(error.stack)
+  this.emit('end')
+}
